d3a20b00575bd721c0c4b7946e955009bb25131f
andreychano <maa.59perm@gmail.com>
2025-11-25 23:13:44 +0500

    refactor: разбить CompressFile на отдельные методы
    - Process: сжатие в памяти
    - CompressAndSave: сжатие + сохранение
    - GetFile: получение файла через репозиторий

--- pkg/core/service/compression.go
+++ pkg/core/service/compression.go
@@ -24,7 +24,7 @@ func NewCompressionService(repo port.FileRepository, processors ...port.Processo
 	}
 }
 
-// CompressFile - основной метод, выполняющий всю работу.
+/* CompressFile - основной метод, выполняющий всю работу. УСТАРЕЛ
 func (s *CompressionService) CompressFile(ctx context.Context, file domain.File, opts domain.Options) (string, error) {
 	var selectedProcessor port.Processor
 	// 1. Найти подходящий процессор
@@ -58,4 +58,59 @@ func (s *CompressionService) CompressFile(ctx context.Context, file domain.File,
 	}
 
 	return savedPath, nil
+}
+*/
+
+//Новые функции делят старую на части и еще добавлятся возврат пути GetFile
+
+//1. Process - "Чистая функция"
+//Только сжимает картинку и возвращает результат в памяти
+//Ничего не сохраняет на диск
+
+func (s *CompressionService) Process(file domain.File, opts domain.Options) (domain.File, error) {
+	var selectedProcessor port.Processor
+
+	//Подбор подходящего процессора компрессии
+
+	for _, p := range s.processors {
+		if p.Supports(file.MimeType) {
+			selectedProcessor = p
+			break
+		}
+	}
+
+	if selectedProcessor == nil {
+		return domain.File{}, fmt.Errorf("unsupport file type: %s", file.MimeType)
+	}
+
+	//Сжатие
+
+	return selectedProcessor.Process(file, opts)
+}
+
+//2. Compress and save - "Полный цикл"
+//Вызывает Process, потом отдает результат в Repository для последующего сохранения.
+
+func (s *CompressionService) CompressAndSave(ctx context.Context, file domain.File, opts domain.Options) (string, error) {
+	//Сжатие, использование метода Process
+	compressedFile, err := s.Process(file, opts)
+	if err != nil {
+		return "", err
+	}
+
+	//Генерируем уникальное имя
+	uniqueID := uuid.New().String()
+	fileName := fmt.Sprintf("%s.%s", uniqueID, opts.Format)
+
+	//Кладем в папку "compressed" внутри хранилища
+	filePath := filepath.Join("compressed", fileName)
+
+	//Сохраняем
+	return s.repository.Save(ctx, compressedFile, filePath)
+}
+
+// 3. GetFile - "Поиск"
+// Просит в repository найти файл
+func (s *CompressionService) GetFile(ctx context.Context, path string) (domain.File, error) {
+	return s.repository.Get(ctx, path)
 }
