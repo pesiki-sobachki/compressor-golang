756fcce90e3b45c4b7eb5da67b5fe499c2192607
andreychano <maa.59perm@gmail.com>
2025-11-25 22:55:37 +0500

    feat: added GET func to internal/adapter/repository/local/storage.go
    - Added Get method to FileRepository interface

--- pkg/core/port/repository.go
+++ pkg/core/port/repository.go
@@ -10,4 +10,7 @@ import (
 
 type FileRepository interface {
 	Save(ctx context.Context, file domain.File, path string) (string, error)
+
+	//Метод ищет файл по указанному пути и возвращает его, если таковой существует
+	Get(ctx context.Context, path string) (domain.File, error)
 }
