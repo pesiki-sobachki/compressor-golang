a7ce59e16aef3373aafaee8ec3d48fc65d84d4b3
andreychano <maa.59perm@gmail.com>
2025-12-20 23:45:12 +0500

    feat: added config.go sturcture

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,36 @@
 package config
 
 type HTTP struct {
-	Address string `conf:"http_address" env:"HTTP_ADDRESS" default:":8080"`
+	Address         string `yaml:"address"`
+	ReadTimeout     string `yaml:"read_timeout"`
+	WriteTimeout    string `yaml:"write_timeout"`
+	IdleTimeout     string `yaml:"idle_timeout"`
+	MaxUploadSizeMB int64  `yaml:"max_upload_size_mb"`
 }
 
 type Storage struct {
-	Path string `conf:"storage_path" env:"STORAGE_PATH" default:"./storage"`
+	Path             string `yaml:"path"`
+	CompressedSubdir string `yaml:"compressed_subdir"`
+	TmpSubdir        string `yaml:"tmp_subdir"`
 }
 
 type Log struct {
-	Level string `conf:"log_level" env:"LOG_LEVEL" default:"info"`
+	Level      string `yaml:"level"`
+	Format     string `yaml:"format"`
+	WithCaller bool   `yaml:"with_caller"`
+}
+
+type Image struct {
+	DefaultFormat  string   `yaml:"default_format"`
+	DefaultQuality int      `yaml:"default_quality"`
+	MaxWidth       int      `yaml:"max_width"`
+	MaxHeight      int      `yaml:"max_height"`
+	AllowFormats   []string `yaml:"allow_formats"`
 }
 
 type Config struct {
-	HTTP    HTTP
-	Storage Storage
-	Log     Log
+	HTTP    HTTP    `yaml:"http"`
+	Storage Storage `yaml:"storage"`
+	Log     Log     `yaml:"log"`
+	Image   Image   `yaml:"image"`
 }
